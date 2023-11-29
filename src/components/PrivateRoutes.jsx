6fa6c5852120ddb29d8725a2b0a38ec6b13d9b70
danishzahid <danishst.johns@gmail.com>
2023-11-29 12:37:28 +0530

    ...

--- src/components/PrivateRoutes.jsx
+++ src/components/PrivateRoutes.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
-// import Home from "../pages/Home";
-// import EditProfile from "../pages/EditProfile";
+import Home from "../pages/Home";
+import EditProfile from "../pages/EditProfile";
 
 function PrivateRoutes() {
   const { auth } = useAuth();
@@ -11,8 +11,8 @@ function PrivateRoutes() {
   if (auth === undefined) return "loading...";
   return auth === true ? (
     <Outlet>
-      {/* <Home path="/" />
-      <EditProfile path="/edit-profile" /> */}
+      <Home path="/" />
+      <EditProfile path="/edit-profile" />
     </Outlet>
   ) : (
     <Navigate to="/auth" />
